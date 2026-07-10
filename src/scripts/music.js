8938497c76097a6eb29f2b91b4f92ecc7ea13570
Colton Williams <cole242000@gmail.com>
2026-07-10 15:07:03 -0400

    refactor: update music modal and cursors

--- src/scripts/music.js
+++ src/scripts/music.js
@@ -1,3 +1,7 @@
 $("#menu-music-button").on("click", () =>
   $("#music-modal").attr("aria-hidden", false),
 );
+
+$("#music-modal-close-button").on("click", () =>
+  $("#music-modal").attr("aria-hidden", true),
+);
