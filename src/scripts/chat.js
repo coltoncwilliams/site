a76818c44389470b7d6724c6458f5441dbb1dc7f
Colton Williams <cole242000@gmail.com>
2026-07-15 19:34:52 -0400

    refactor: add chat micro interactions

--- src/scripts/chat.js
+++ src/scripts/chat.js
@@ -1,25 +1,76 @@
 import { isSmallScreenSize } from "./utils";
 import { setPortraitMinimized } from "./portrait";
+import { DEFAULT_CHATS, DEFAULT_CHAT_TIME_MS } from "./constants";
 
-$("#chat").on("click", clickChat);
+let chatLoading = false;
+let defaultChatLoading = false;
+let chats = JSON.parse(localStorage.getItem("chats")) || [];
+let renderedDefaultChats =
+  JSON.parse(localStorage.getItem("defaultChats")) || [];
 
-const yourChats = [];
+renderedDefaultChats.forEach((chatMsg) =>
+  renderChat({ role: "assistent", content: chatMsg }),
+);
+chats.forEach(renderChat);
+scrollToBottom();
 
-$("#chat-form").on("submit", function (e) {
-  e.preventDefault();
-  const chat = $("#chat-input").val();
-  if (!chat) {
+if (!chats.length) {
+  const defaultChatsToRender = sendNextDefaultChat();
+  let timeout = DEFAULT_CHAT_TIME_MS;
+  defaultChatsToRender.forEach((defaultChatMsg) => {
+    setTimeout(() => {
+      if (!chats.length) {
+        sendNextDefaultChat();
+      }
+    }, timeout);
+    timeout = timeout + DEFAULT_CHAT_TIME_MS;
+  });
+}
+
+function sendNextDefaultChat() {
+  setChatLoading(false, true);
+  const renderedDefaultChatsSet = new Set(renderedDefaultChats);
+  const defaultChatsToRender = DEFAULT_CHATS.filter(
+    (item) => !renderedDefaultChatsSet.has(item),
+  );
+  if (defaultChatsToRender.length) {
+    renderChat({ role: "assistent", content: defaultChatsToRender[0] });
+    renderedDefaultChats.push(defaultChatsToRender.shift());
+    localStorage.setItem("defaultChats", JSON.stringify(renderedDefaultChats));
+    if (defaultChatsToRender.length) {
+      setChatLoading(true, true);
+    }
+    scrollToBottom();
+  }
+  return defaultChatsToRender;
+}
+
+function renderChat(chatObj, id, loading = false) {
+  const idStr = `id="${id}"`;
+  if (chatObj.role === "user") {
+    $("#chat-conversation").append(
+      `<div class="chat-you" ${id && idStr}><p class="header">You:</p><p class="content">${chatObj.content}</p></div>`,
+    );
     return;
   }
-  $("#chat-input").val("");
-  $("#chat-conversation").animate(
-    {
-      scrollTop: $("#chat-conversation")[0].scrollHeight,
-    },
-    300,
+  $("#chat-conversation").append(
+    `<div class="chat-me" ${id && idStr}><p class="header">Me:</p><p class="content ${loading && "after:animate-ellipsis after:content-['.']"}">${chatObj.content}</p></div>`,
   );
-  sendChat(chat);
-});
+}
+
+function sendChat(chatMsg) {
+  if (defaultChatLoading) {
+    setChatLoading(false, true);
+  }
+  const chatObj = { role: "user", content: chatMsg };
+  renderChat(chatObj);
+  setChatLoading(true);
+  chats.push(chatObj);
+  localStorage.setItem("chats", JSON.stringify(chats));
+
+  scrollToBottom();
+  setTimeout(() => setChatLoading(false), 10000);
+}
 
 function clickChat() {
   $("#chat-input").focus();
@@ -34,12 +85,25 @@ function clickChat() {
   }
 }
 
-function sendChat(chat) {
-  yourChats.push(chat);
-  $("#chat-conversation").append(
-    `<div class="chat-you"><p class="header">You:</p><p class="content">${chat}</p></div>`,
-  );
+function setChatLoading(loading, defaultChat = false) {
+  if (defaultChat) {
+    defaultChatLoading = loading;
+  } else {
+    chatLoading = loading;
+    $("#chat-submit").attr("aria-disabled", loading);
+  }
+
+  if (loading) {
+    return renderChat(
+      { role: "assitent", content: "" },
+      "chat-loading-div",
+      true,
+    );
+  }
+  $("#chat-loading-div").remove();
 }
+
+$("#chat").on("click", clickChat);
 
 $("#chat").on("focusin", function () {
   if (isSmallScreenSize()) {
@@ -64,3 +128,32 @@ $("#chat").on("focusout", function (e) {
     }
   }, 500);
 });
+
+$("#chat-form").on("submit", function (e) {
+  e.preventDefault();
+  const chat = $("#chat-input").val();
+  if (!chat || chatLoading) {
+    return;
+  }
+  $("#chat-input").val("");
+  sendChat(chat);
+});
+
+$("#chat-button-clear").on("click", () => {
+  chats = [];
+  localStorage.setItem("chats", JSON.stringify(chats));
+  $("#chat-conversation").empty();
+  renderedDefaultChats.forEach((chatMsg) =>
+    renderChat({ role: "assistent", content: chatMsg }),
+  );
+  scrollToBottom();
+});
+
+function scrollToBottom() {
+  $("#chat-conversation").animate(
+    {
+      scrollTop: $("#chat-conversation")[0].scrollHeight,
+    },
+    300,
+  );
+}
---
+++ src/scripts/constants.js
@@ -0,0 +1,7 @@
+export const DEFAULT_CHATS = [
+  "Heyy.. welcome to my website. Ask me anything about myself :)",
+  "You can say something like: 'Tell me about your expertise with Django'",
+  "Or just ask me what my hobbies are 🎨",
+];
+
+export const DEFAULT_CHAT_TIME_MS = 10000;
